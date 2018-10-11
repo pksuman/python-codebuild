167812a1d157cbe4eca31c091e4fa10c18c51b68
Prabhat Kumar Suman <prabhat5052@gmail.com>
2018-10-11 19:00:02 +0530

    modified file

--- HelloWorld_tst.py
+++ HelloWorld_tst.py
@@ -4,6 +4,7 @@ from HelloWorld import HelloWorld
 class MyTestCase(unittest.TestCase):
   def test_default_greeting_set(self):
     hw = HelloWorld()
+    print("I am modified")
     self.assertEqual(hw.message, 'Hello world!')
 
 if __name__ == '__main__':
